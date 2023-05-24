18e0e42c9395d2c901396e478a3240b3ebeb5c60
samalexesqu <76229276+samalexesqu@users.noreply.github.com>
2023-05-23 23:00:33 -0400

    sam
    added metrics

--- ugh.R
+++ ugh.R
@@ -7,20 +7,17 @@ library(stringr)
 library(stringi)
 library(lubridate)
 
-
 library(ggmap)
-
 library(ggplot2)
-
 library(caTools)
 
-setwd('/Users/emmaboudreau/Documents/GitHub/697proj/')
-#setwd('/Users/samuelesquivel/Documents/GitHub/697project/')
+#setwd('/Users/emmaboudreau/Documents/GitHub/697proj/')
+setwd('/Users/samuelesquivel/Documents/GitHub/697project/')
 
+
+###PREP/MUNGE###
 # read in the data-----
 data = read.csv('export.csv')
-
-
 #---- 1 dataframe we could use
 data2 = select(data,-c(dist_dirc_exit, age, max_injr_svrty_cl,
                        numb_fatal_injr, numb_nonfatal_injr,injy_stat_descr,
@@ -70,23 +67,25 @@ data3 = data2%>%
   mutate(season = ifelse(date>=WS | date<SE, "0", ifelse(date>=SE & date< SS, "1",
                                                          ifelse(date>=SS&date< FE, "2", "3"))))
 
-
-###REGARDING TIME###
-#step 1
 #convert existing date and time to standard POSIX format
 data3 <- data3 %>%
   mutate(crash_time_2 = str_replace(crash_time_2, "\\s(AM|PM)", " \\1"),  # Remove the space before AM/PM
          crash_time_2 = as.POSIXct(crash_time_2, format = "%I:%M %p"),  # Convert to POSIXct format
          crash_date = as.Date(date))  # Convert 'date' to Date format
-
+#mutate data3 to standard time 
 data3 <- data3 %>%
   mutate(crash_date_time_standard = as.POSIXct(paste(crash_date, format(crash_time_2, "%H:%M:%S")), format = "%Y-%m-%d %H:%M:%S"))
-#step 2
 #drop the excess columns we don't need anymore and make new polished data frame
 data4 <- data3 %>%
   select(-crash_date, -date, -crash_time_2,-crash_numb,-city_town_name)
 #drop_na2()%>% #drop any row with NA
-#step 3
+
+
+
+
+
+###BASIC ANALYSIS###
+###Crash count vs time of day###
 #do some analysis on the timing of events
 #extract the hour from the crash_date_time_standard column
 data4 <- data4 %>%
@@ -97,8 +96,7 @@ crash_count <- data4 %>%
 #plot the crash count by hour
 plot(crash_count$hour, crash_count$n, type = "l", xlab = "Hour of Day", ylab = "Crash Count", main = "Crash Count by Hour")
 
-
-###REGARDING LOCATION###
+###Location in Boston map###
 # Load required libraries
 library(ggplot2)
 library(ggmap)
@@ -119,8 +117,13 @@ ggmap(boston_map) +
 
 
 
-#---- logistic regression
 
+
+
+
+
+###MODEL ANALYSIS###
+#---- logistic regression
 data4 <- na.omit(data4)
 
 # Loading caret library
@@ -134,11 +137,7 @@ logistic_model <- glm(severity ~ ., family = binomial(), train)
 # Checking the model
 summary(logistic_model)
 
-
 pred_prob_lg <- predict(logistic_model, test, type = "response")
-
-
-
 
 train$pred_class <- ifelse(logistic_model$fitted.values>=0.5, "Yes", "No")
 # Generating the classification table
@@ -151,13 +150,10 @@ test$pred_class <- ifelse(pred_prob_lg >= 0.5, "Yes", "No")
 ctab_test <- table(test$severity, test$pred_class)
 ctab_test
 
-
-
 #Accuracy = (TP + TN)/(TN + FP + FN + TP)
 # Accuracy in Training dataset
 accuracy_train <- sum(diag(ctab_train))/sum(ctab_train)*100
 accuracy_train
-
 
 # Accuracy in Test dataset
 accuracy_test <- sum(diag(ctab_test))/sum(ctab_test)*100
@@ -165,7 +161,6 @@ accuracy_test
 
 Recall <- (ctab_train[2, 2]/sum(ctab_train[2, ]))*100
 Recall
-
 
 TNR <- (ctab_train[1, 1]/sum(ctab_train[1, ]))*100
 TNR
@@ -175,38 +170,73 @@ Precision
 
 F_Score <- (2 * Precision * Recall / (Precision + Recall))/100
 F_Score
-
-
-
 #library(pROC)
 #roc <- roc(train$Class, logistic_model$fitted.values)
 #auc(roc)
 
 
 
-#---- SVM
+#---- SVM-Radial 
 library(e1071)
+# Split the data into training and testing sets
+set.seed(123)  # For reproducibility
+train_indices <- sample(1:nrow(data4), 0.7 * nrow(data4))  # 70% for training
+train_data <- data4[train_indices, ]
+test_data <- data4[-train_indices, ]
 
-
-svm_model <- svm(formula = severity ~ ., kernel = "radial", data = train)
-
-
-y_pred = predict(svm_model, newdata = test)
-
-y_pred = ifelse(y_pred>=.5, "1","0")
-
-a = 100*mean(y_pred==test[,1])
-
-accuracy <- sum(y_pred == test$severity) / nrow(test)
+# Train the SVM model
+svm_model <- svm(formula = severity ~ ., kernel = "radial", data = train_data)
+# Make predictions on the test data
+y_pred <- predict(svm_model, newdata = test_data)
+y_pred <- ifelse(y_pred >= 0.5, "1", "0")
+# Calculate accuracy
+accuracy <- sum(y_pred == test_data$severity) / nrow(test_data)
 print(paste("Accuracy:", accuracy))
+# Calculate confusion matrix
+confusion <- table(Actual = test_data$severity, Predicted = y_pred)
+print("Confusion Matrix:")
+print(confusion)
+# Calculate precision, recall, and F1-score
+#precision <- confusion[2, 2] / sum(confusion[, 2])
+#recall <- confusion[2, 2] / sum(confusion[2, ])
+f1_score <- 2 * precision * recall / (precision + recall)
+print(paste("Precision:", precision))
+print(paste("Recall:", recall))
+print(paste("F1-Score:", f1_score))
+# Calculate AUC-ROC (if applicable)
+if (length(levels(test_data$severity)) == 2) {
+  library(pROC)
+  roc_obj <- roc(test_data$severity, as.numeric(y_pred))
+  auc_roc <- auc(roc_obj)
+  print(paste("AUC-ROC:", auc_roc))
+}
+# Calculate training accuracy
+train_predictions <- predict(svm_model, newdata = train_data)
+train_accuracy <- sum(train_predictions == train_data$severity) / nrow(train_data)
+print(paste("Training Accuracy:", train_accuracy))
+
+# Calculate testing accuracy
+test_accuracy <- sum(y_pred == test_data$severity) / nrow(test_data)
+print(paste("Testing Accuracy:", test_accuracy))
+
+# Calculate True Negative Rate (TNR)
+TN <- confusion[1, 1]  # True negatives
+FP <- confusion[1, 2]  # False positives
+TNR <- TN / (TN + FP)
+print(paste("TNR:", TNR))
 
 
 
 
 
 
+
+
+
+
+
+###SUBSEQUENT ANALYSIS###
 #---- GGplotsss
-
 season_counts <- data4 %>%
   group_by(season, severity) %>%
   summarise(count = n()) %>%
@@ -243,7 +273,6 @@ weather_counts$weather <- case_when(
   weather_counts$weather == 2 ~ "Rain",
   weather_counts$weather == 3 ~ "Snow"
 )
-
 #bar plot
 ggplot(weather_counts, aes(x = weather, y = count, fill = as.factor(severity))) +
   geom_bar(stat = "identity") +
@@ -253,3 +282,9 @@ ggplot(weather_counts, aes(x = weather, y = count, fill = as.factor(severity))) 
   scale_fill_brewer(palette = "Paired",
                     labels = c("Non-Injury", "Injury")) +
   theme_minimal()
+
+
+
+
+
+
